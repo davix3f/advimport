af58f9d6716592007580419dcef8b8b72742a243
Davide <davide_fiorito@libero.it>
2018-08-13 01:31:57 +0200

    Added setup.py

---
+++ src/setup.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name = "advimport",
+    url = "https://github.com/davix3f/advimport",
+    author = "davix3f",
+    author_email = "davide_fiorito@libero.it",
+    version = "0.1",
+    packages = find_packages();
+    license = "GNU GPL v3",
+    include_package_data = False
+)
