bcd749b01fffde0ab62f4ef57b2d713b63d73cd6
Davide <davide_fiorito@libero.it>
2018-08-13 03:25:12 +0200

    Added description

--- src/setup.py
+++ src/setup.py
@@ -7,6 +7,7 @@ setup(
     author = "davix3f",
     author_email = "davide_fiorito@libero.it",
     version = "0.1",
+    description = "Better and easier imports",
     packages = find_packages(),
     license = "GNU GPL v3",
     include_package_data = False
